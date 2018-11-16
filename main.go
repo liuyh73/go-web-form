942cb2521d232a7204cd894d215b041143731ce9
liuyh73 <15989067460@163.com>
2018-11-17 00:11:31 +0800

    modify main.go

--- main.go
+++ main.go
@@ -1,53 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "io"
-    "log"
-    "net/http"
-    "os"
+	"os"
+
+	"github.com/liuyh73/go-web-form/service"
+	flag "github.com/spf13/pflag"
 )
 
-func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("method:", r.Method)
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("login.gtpl")
-        log.Println(t.Execute(w, nil))
-    } else {
-        r.ParseForm()
-        t, _ := template.New("login").Parse(`{{define "username"}}Hello, {{.}}!{{end}}`)
-        log.Println(t.ExecuteTemplate(w, "username", r.Form.Get("username")))
-    }
-}
-
-func upload(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("method: ", r.Method)
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("upload.gtpl")
-        t.Execute(w, nil)
-    } else {
-        r.ParseMultipartForm(32 << 20)
-        file, handler, err := r.FormFile("uploadfile")
-        defer file.Close()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        fmt.Fprintf(w, "%v", handler.Header)
-
-        f, err := os.OpenFile("./file/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-        defer f.Close()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        io.Copy(f, file)
-    }
-}
+const (
+	PORT string = "8080"
+)
 
 func main() {
-    http.HandleFunc("/login", login)
-    http.HandleFunc("/upload", upload)
-    http.ListenAndServe(":9090", nil)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = PORT
+	}
+
+	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+	flag.Parse()
+	if len(*pPort) != 0 {
+		port = *pPort
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
 }
