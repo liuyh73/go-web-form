88107697878d4d660cc769d568de43e47948a485
liuyh73 <15989067460@163.com>
2018-11-17 12:08:40 +0800

    add comment

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+// 定义默认端口
 const (
 	PORT string = "8080"
 )
 
 func main() {
+	// 获取用户自定义端口号
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = PORT
@@ -22,7 +24,8 @@ func main() {
 	if len(*pPort) != 0 {
 		port = *pPort
 	}
-
+	// 创建应用服务
 	server := service.NewServer()
+	// 运行该服务，监听port端口
 	server.Run(":" + port)
 }
